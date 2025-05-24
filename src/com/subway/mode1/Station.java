dcaf1a98d81eb3027a53b30b1892a9f571694ea1
syx <627902779@qq.com>
2025-05-24 18:57:17 +0800

    创建车站类

---
+++ src/com/subway/mode1/Station.java
@@ -0,0 +1,28 @@
+package com.subway.mode1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Station {
+	    private String name;
+	    private Set<String> lines = new HashSet<>();
+
+	    public Station(String name) {
+	        this.name = name;
+	    }
+
+	    public void addLine(String line) {
+	        lines.add(line);
+	    }
+
+	    // Getters
+	    public String getName() { return name; }
+	    public Set<String> getLines() { return lines; }
+
+	    @Override
+	    public String toString() {
+	        return "<" + name + ", " + lines + ">";
+	    }
+	
+
+}
